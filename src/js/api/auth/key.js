76a1348a4bee666502dce1540ae8be14b29e580e
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-09-10 16:39:04 +0200

    created getkey function

--- src/js/api/auth/key.js
+++ src/js/api/auth/key.js
@@ -1,1 +1,17 @@
-export async function getKey(name) {}
+export async function getKey(name) {
+    //call for api to generate a api key for the user
+    const response = await fetch('https://v2.api.noroff.dev/auth/create-api-key', {
+        method: 'POST',
+        body: JSON.stringify({ name }),
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error('failed to create api key');
+    }
+
+    const data = await response.json();
+    return data.apiKey;
+}
