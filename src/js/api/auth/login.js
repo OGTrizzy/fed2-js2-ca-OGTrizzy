c35eba21643fc4903971afd5920936a1698445da
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-09-10 16:43:19 +0200

    created api login function

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,16 @@
-export async function login({ email, password }) {}
+export async function login({ email, password }) {
+    const response = await fetch('https://v2.api.noroff.dev/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (!response.ok){
+        throw new Error('login failed');
+      }
+
+      const data = await response.json();
+      return data;
+}
