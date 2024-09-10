7e76cf8eb2de2617602a35a91ac058a781949b33
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-09-10 17:01:42 +0200

    created function for auth/register.js

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -5,4 +5,19 @@ export async function register({
   bio,
   banner,
   avatar,
-}) {}
+}) {
+  const response = await fetch('https://v2.api.noroff.dev/auth/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type':  'application/json',
+    },
+    body: JSON.stringify({name, email, password, bio, banner, avatar})
+  });
+
+  if (!response.ok){
+    throw new Error('register failed');
+  }
+
+  const data = await response.json();
+  return data;
+}
