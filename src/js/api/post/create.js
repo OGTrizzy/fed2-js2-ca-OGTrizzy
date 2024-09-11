a562bacadb0bd8a1cc6787682ba9d60b2089d4d9
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-09-11 09:57:53 +0200

    created function for post/create.js

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,16 @@
-export async function createPost({ title, body, tags, media }) {}
+export async function createPost({ title, body, tags, media }) {
+    const response = await fetch ('https://v2.api.noroff.dev/blog/posts', {
+        method:  'POST',
+        headers: {
+            'Content-Type':  'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({title, body, tags, media})
+    });
+    if (!response.ok){
+        throw new Error('failed to create post');
+    }
+    
+    const data = await response.json();
+    return data;
+}
