264019f6b7bb3e136c2d1ea4c7568ad1f33ac65b
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-09-11 10:06:48 +0200

    created function on post/update.js

--- src/js/api/post/update.js
+++ src/js/api/post/update.js
@@ -1,1 +1,17 @@
-export async function updatePost(id, { title, body, tags, media }) {}
+export async function updatePost(id, { title, body, tags, media }) {
+    const response = await fetch (`https://v2.api.noroff.dev/blog/posts/${postId}`,{
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify({title, body, tags, media})
+    });
+
+    if (!response.ok) {
+        throw new Error(`failed to update post`);
+    }
+
+    const data = await response.json();
+    return data;
+}
