dcf02ed281f31ecc1ee931f36e96868bd74d8ff7
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-11-09 22:03:37 +0100

    damn, i forgot to commit :) i did so much as u can see

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import { onCreatePost } from "./js/ui/post/create.js";
 import { onLogin } from '../src/js/ui/auth/login.js';
 import router from "./js/router/index.js";
+import 'bootstrap';
+
 //import { getKey } from "./js/api/auth/key.js";
 
 window.addEventListener('load', () => {
