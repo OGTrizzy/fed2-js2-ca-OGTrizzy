a97512b56e4ec3f4d7644c2022e9181540d8ab7c
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-11-10 02:43:30 +0100

    Updates

--- assets/login-BSLx2fCt.js
+++
@@ -1,1 +0,0 @@
-import{o}from"./style-Djx7_ml-.js";const n=document.forms.login;n.addEventListener("submit",o);
--- assets/postCreate-Bi21x01d.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./style-Djx7_ml-.js";import{a as t}from"./authGuard-DDO8JLiG.js";t();const o=document.forms["create-post"];o?o.addEventListener("submit",r):console.error("Form not found");
