57aafd4661ebb7cf00ff178d6cf606ee550f2cc2
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2025-06-06 15:28:35 +0200

    added login error fix, reason i didnt post earlier is most likely because of the deadline

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,6 +1,6 @@
 export function authGuard() {
   if (!localStorage.accessToken) {
     alert("You must be logged in to view this page");
-    window.location.href = "/auth/login/";
+    window.location.href = "/fed2-js2-ca-OGTrizzy/auth/login/";
   }
 }
